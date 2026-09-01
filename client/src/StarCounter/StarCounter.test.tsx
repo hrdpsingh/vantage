e10bda8c70d997f768cd0113a18657a2c5582ce5
hrdpsingh <harshdeep.s.rakkar@gmail.com>
2026-09-01 14:45:37 +0000

    test(client): add tests for various inputs
    - Add test to check correct username and repository.
    - Add test to check non-existent username and repository.

--- client/src/StarCounter/StarCounter.test.tsx
+++ client/src/StarCounter/StarCounter.test.tsx
@@ -1,13 +1,66 @@
+import userEvent from "@testing-library/user-event";
 import { render, screen } from "@testing-library/react";
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, vi } from "vitest";
 import { StarCounter } from "./StarCounter";
 
 describe("StarCounter", () => {
-  it("renders inputs and submit button on initial load", () => {
+  it("renders input fields and submit button on initial load", () => {
     render(<StarCounter />);
 
     expect(screen.getByPlaceholderText(/owner/i)).toBeInTheDocument();
     expect(screen.getByPlaceholderText(/repository/i)).toBeInTheDocument();
     expect(screen.getByRole("button", { name: /fetch/i })).toBeInTheDocument();
+  });
+
+  it("provides valid username and repository and checks result", async () => {
+    const fetchSpy = vi.spyOn(globalThis, "fetch").mockResolvedValue({
+      ok: true,
+      status: 200,
+      json: async () => ({ stargazers_count: 1000 }),
+    } as Response);
+
+    render(<StarCounter />);
+
+    const username = screen.getByPlaceholderText(/owner/i);
+    const repository = screen.getByPlaceholderText(/repository/i);
+    const button = screen.getByRole("button", { name: /fetch/i });
+
+    await userEvent.type(username, "hrdpsingh");
+    await userEvent.type(repository, "insight");
+    await userEvent.click(button);
+
+    expect(fetchSpy).toHaveBeenCalledTimes(1);
+    expect(fetchSpy).toHaveBeenCalledWith(
+      "http://127.0.0.1:8000/repository/hrdpsingh/insight/stars",
+    );
+
+    expect(await screen.findByText(/Stars: 1000/)).toBeInTheDocument();
+  });
+
+  it("provides invalid username and repository and checks result", async () => {
+    const fetchSpy = vi.spyOn(globalThis, "fetch").mockResolvedValue({
+      ok: true,
+      status: 404,
+      json: async () => ({ status_code: 404, detail: "Repository not found" }),
+    } as Response);
+
+    render(<StarCounter />);
+
+    const username = screen.getByPlaceholderText(/owner/i);
+    const repository = screen.getByPlaceholderText(/repository/i);
+    const button = screen.getByRole("button", { name: /fetch/i });
+
+    await userEvent.type(username, "hrdpsingh");
+    await userEvent.type(repository, "react");
+    await userEvent.click(button);
+
+    expect(fetchSpy).toHaveBeenCalledTimes(1);
+    expect(fetchSpy).toHaveBeenCalledWith(
+      "http://127.0.0.1:8000/repository/hrdpsingh/react/stars",
+    );
+
+    expect(
+      await screen.findByText(/Repository not found./),
+    ).toBeInTheDocument();
   });
 });
